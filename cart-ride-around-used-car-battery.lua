3faf8832eade543bf7d70f349338cd00afdde294
SchollerScripts <109981414+SchollerScripts@users.noreply.github.com>
2022-07-31 14:28:10 +0200

    having too much fun with this script
    cleaned code : 
        - replaced wait with task.wait, task.spawn with task.defer
        - added BindableEvents
        - rewrote a lot of functions
        - now using while .. instead of repeat..
    god function now waits for added falling rails and pendulums instead of looping through the workspace every 2.5 seconds
    collectcoins function is now slower, but better optimized and will not throw errors

--- cart-ride-around-used-car-battery.lua
+++ cart-ride-around-used-car-battery.lua
@@ -1,18 +1,13 @@
 local player = game.Players.LocalPlayer
 local uis = game:GetService("UserInputService")
+local runs = game:GetService("RunService")
 local ws = 16
 local jp = 50
 
-getgenv().infjump = false
-getgenv().collectcoins = false
-
-getgenv().goforward = false
-getgenv().gobackwards = false
-getgenv().stopCarts = false
-getgenv().spamspawn = false
 getgenv().bombtoo = false
-getgenv().lightCarts = false
-getgenv().god = false
+
+local characterEvent = Instance.new("BindableEvent")
+local infJumpEvent = Instance.new("BindableEvent")
 
 local sgui = game:GetService("StarterGui")
 local function notify(title, text, dur, cb)
@@ -34,17 +29,17 @@ end
 
 local function getChar()
     if not checkChar() then
-        repeat
-            wait()
-        until checkChar()
+        characterEvent.Event:Wait()
+        task.wait()
     end
     return player.Character
 end
 
 player.CharacterAdded:Connect(function(chr)
-    repeat wait() until checkChar()
-    chr.Humanoid.WalkSpeed = ws
-    chr.Humanoid.JumpHeight = jp
+    task.wait()
+    characterEvent:Fire()
+    getChar().Humanoid.WalkSpeed = ws
+    getChar().Humanoid.JumpHeight = jp
 end)
 
 local teleports = {
@@ -55,9 +50,7 @@ local teleports = {
 }
 
 local function tp(where)
-    task.spawn(function()
-        getChar().HumanoidRootPart.CFrame = teleports[where]
-    end)
+    getChar().HumanoidRootPart.CFrame = teleports[where]
 end
 
 local function tpToSpawn()
@@ -76,264 +69,282 @@ local function tpToShop()
     tp("shop")
 end
 
+local infJump
 local function infiniteJump()
-    task.spawn(function()
-        local a = uis.InputBegan:Connect(function(inp)
-            if inp.KeyCode == Enum.KeyCode.Space then
-                getChar().Humanoid:ChangeState(3)
-            end 
-        end)
-        repeat wait() until not getgenv().infjump
-        a:Disconnect()
-    end)
-end
-
-local function collectCoins()
-    task.spawn(function()
-        while getgenv().collectcoins do
-            for i,v in pairs(workspace.coinspawner:GetDescendants()) do
-                if v.Name == "TouchInterest" and v.Parent and v.Parent.Parent and v.Parent.Parent:IsA("Tool") then
-                    firetouchinterest(getChar().Head, v.Parent, 0)
-                end
-                wait()
-            end
-            wait(.3)
-            local currtool = getChar():FindFirstChildOfClass("Tool")
-            if currtool and string.lower(currtool.Name):find("coin") then
-                currtool:Activate()
-            end
-            wait(.5)
-            for i,v in pairs(player.Backpack:GetChildren()) do
-                if v.Parent ~= nil and v:IsA("Tool") and string.lower(v.Name):find("coin") then
-                    getChar().Humanoid:EquipTool(v)
-                    wait(.3)
-                    v:Activate()
-                end
-                wait(.3)
-            end
-            wait(3)
+    infJump = uis.InputBegan:Connect(function(inp)
+        if inp.KeyCode == Enum.KeyCode.Space then
+            getChar().Humanoid:ChangeState(3)
         end 
     end)
 end
 
-local function getTools()
-    task.spawn(function()
-        local hrp = getChar().HumanoidRootPart
-        local pos = hrp.CFrame
-        local giver = workspace:FindFirstChild("Giver")
-        firetouchinterest(hrp, giver, 0)
-        wait()
-        firetouchinterest(hrp, giver, 1)
-        repeat wait() until (hrp.Position - Vector3.new(-494.5295715332031, 181.86376953125, 589.2139282226562)).Magnitude < 3
-        hrp.CFrame = pos
-    end)
-end
-
-local function goForward()
-    task.spawn(function()
-        for _,v in pairs(workspace:GetChildren()) do
-            if v.Name:lower():find("cart") and v:FindFirstChild("forward") and v.forward:FindFirstChild("ClickDetector") then
-                fireclickdetector(v.forward.ClickDetector)
-            end
-        end
-    end)
-end
-
-local function goBackwards()
-    task.spawn(function()
-        for _,v in pairs(workspace:GetChildren()) do
-            if v.Name:lower():find("cart") and v:FindFirstChild("backward") and v.backward:FindFirstChild("ClickDetector") then
-                fireclickdetector(v.backward.ClickDetector)
-            end
-        end
-    end)
-end
-
-local function stopCarts()
-    task.spawn(function()
-        for _,v in pairs(workspace:GetChildren()) do
-            if v.Name:lower():find("cart") and v:FindFirstChild("stop") and v.stop:FindFirstChild("ClickDetector") then
-                fireclickdetector(v.stop.ClickDetector)
-            end
-        end
-    end)
-end
-
-local function lightCarts()
-    task.spawn(function()
-        for _,v in pairs(workspace:GetChildren()) do
-            if v.Name:lower():find("cart") and v:FindFirstChild("lightbutton") and v.lightbutton:FindFirstChild("ClickDetector") then
-                fireclickdetector(v.lightbutton.ClickDetector)
-            end
-        end
-    end)
-end
-local function god()
-    task.spawn(function()
-        local s
-        notify("God toggled", "God has been toggled.", 3)
-        while getgenv().god do
-            for _,v in pairs(workspace:GetChildren()) do
-                if v.Name == "ElectrifiedRail" or v.Name == "garrote wire" or v.Name == "railspawner" or v.Name == "spring gun" or v.Name == "Pendulum" then
-                    repeat s = v:FindFirstChild("TouchInterest", true); if s then s:Destroy() end until not v:FindFirstChild("TouchInterest", true)
+local collectCoinsCon1, collectCoinsCon2
+local function collectCoins(dis)
+    if dis then if collectCoinsCon1 then collectCoinsCon1:Disconnect() end if collectCoinsCon2 then collectCoinsCon2:Disconnect() end return end
+    local bindevent1, deb, child1 = Instance.new("BindableEvent"), false
+    for _,v in pairs(workspace.coinspawner:GetDescendants()) do
+        if v:IsA("Tool") and v.Name:lower():find("coin") then
+            v:WaitForChild("Handle", 5)
+            collectCoinsCon2 = getChar().ChildAdded:Connect(function(ch)
+                if ch == child1 then
+                    bindevent1:Fire()
+                    collectCoinsCon2:Disconnect()
                 end
-            end
-            wait(2.5)
+            end)
+            child1 = v
+            firetouchinterest(getChar().HumanoidRootPart, v.Handle, 0)
+            bindevent1.Event:Wait()
+        end
+        task.wait()
+    end
+    while getChar():FindFirstChildOfClass("Tool") do
+        getChar().Humanoid:UnequipTools()
+        task.wait()
+    end
+    task.wait()
+    for _,v in pairs(player.Backpack:GetChildren()) do
+        if v:IsA("Tool") and v.Name:lower():find("coin") then
+            v.Parent = getChar()
+            task.wait()
+            collectCoinsCon2 = getChar().ChildRemoved:Connect(function(ch)
+                if ch == child1 then
+                    bindevent1:Fire()
+                    collectCoinsCon2:Disconnect()
+                end
+            end)
+            child1 = v
+            v:Activate()
+            bindevent1.Event:Wait()
+        end
+        task.wait()
+    end
+    task.wait()
+    collectCoinsCon1 = workspace.coinspawner.DescendantAdded:Connect(function(desc)
+        if deb then return end
+        deb = true
+        task.wait(2)
+        collectCoinsCon1:Disconnect()
+        if collectCoinsCon2 then collectCoinsCon2:Disconnect() end
+        collectCoins()
+    end)
+end
+
+local giver
+local function findGiver()
+    for _,v in pairs(workspace:GetChildren()) do
+        if v.Name == "Giver" and v.CFrame == CFrame.new(-630.957458, 157.888062, 700.162354, 0, 0, 1, 0, 1, -0, -1, 0, 0) then
+            return v
+        end
+    end
+end
+
+local function getTools()
+    local hrp = getChar().HumanoidRootPart
+    local pos = hrp.CFrame
+    if not giver then giver = findGiver() end
+    firetouchinterest(hrp, giver, 0)
+    task.wait()
+    firetouchinterest(hrp, giver, 1)
+    local rstepped
+    rstepped = runs.RenderStepped:Connect(function()
+        if (hrp.Position - Vector3.new(-29.10, 75.53, 929.93)).Magnitude < 5 then
+            task.wait()
+            hrp.CFrame = pos
+            rstepped:Disconnect()
         end
     end)
+end
+
+local function cartInteract(buttonName)
+    for _,v in pairs(workspace:GetChildren()) do
+        if v.Name:lower():find("cart") and v:FindFirstChild(buttonName) and v[buttonName]:FindFirstChild("ClickDetector") then
+            fireclickdetector(v[buttonName].ClickDetector)
+        end
+    end
+end
+
+local godCons = {}
+local function god()
+    local s
+    notify("God toggled", "God has been toggled.", 3)
+    for _,v in pairs(workspace:GetChildren()) do
+        if v.Name == "ElectrifiedRail" or v.Name == "garrote wire" or v.Name == "railspawner" or v.Name == "spring gun" or v.Name == "Pendulum" then
+            local s = v:FindFirstChild("TouchInterest", true)
+            while s do 
+                s:Destroy()
+                s = v:FindFirstChild("TouchInterest", true)
+            end
+            if v.Name == "railspawner" then
+                table.insert(godCons, v.DescendantAdded:Connect(function(ch)
+                    if ch.Name == "TouchInterest" then task.wait(); ch:Destroy() end
+                end))
+            end
+        end
+    end
+    table.insert(godCons, workspace.ChildAdded:Connect(function(ch)
+        if ch.Name == "Pendulum" then
+            task.wait()
+            local s = ch:FindFirstChild("TouchInterest", true)
+            while s do
+                s:Destroy()
+                s = ch:FindFirstChild("TouchInterest", true)
+            end 
+        end
+    end))
 end
 
 local function spawnCarts()
-    task.spawn(function()
-        for _,v in pairs(workspace:GetChildren()) do
-            if v.Name == "respawner" or v.Name == "rustyrespawner" or (getgenv().bombtoo and v.Name == "bombrespawnerply") then
-                firetouchinterest(getChar().HumanoidRootPart, v.respawn, 0)
-                wait(0.05)
-                firetouchinterest(getChar().HumanoidRootPart, v.respawn, 1)
-            end
+    for _,v in pairs(workspace:GetChildren()) do
+        if v.Name == "respawner" or v.Name == "rustyrespawner" or (getgenv().bombtoo and v.Name == "bombrespawnerply") then
+            firetouchinterest(getChar().HumanoidRootPart, v.respawn, 0)
+            task.wait(0.05)
+            firetouchinterest(getChar().HumanoidRootPart, v.respawn, 1)
         end
-    end)
+    end
 end
 
-task.spawn(function()
-    wait()
-    local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
-    local venyx = lib.new("cart ride around used car battery hax")
-    local mainPage = venyx:addPage("Main")
+local function runUntilFired(func, btnName)
+    local c2,c1,c3 = Instance.new("BindableEvent")
+    c1 = runs.RenderStepped:Connect(function() func(btnName) end)
+    c3 = c2.Event:Connect(function()
+        c1:Disconnect()
+        c3:Disconnect()
+    end)
+    return c2
+end
 
-    local guiSection = mainPage:addSection("UI")
-    local playerSection = mainPage:addSection("Player")
-    local teleportSection = mainPage:addSection("Teleports")
+task.wait()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local venyx = lib.new("cart ride")
+local mainPage = venyx:addPage("Main")
 
-    teleportSection:addButton("Teleport to End", tpToEnd)
-    teleportSection:addButton("Teleport to Shop", tpToShop)
-    teleportSection:addButton("Teleport to Spawn", tpToSpawn)
-    teleportSection:addButton("Teleport to Pit stop", tpToPitStop)
+local guiSection = mainPage:addSection("UI")
+local playerSection = mainPage:addSection("Player")
+local teleportSection = mainPage:addSection("Teleports")
 
-    guiSection:addKeybind("Toggle menu", Enum.KeyCode.G, function()
-        venyx:toggle()
-    end)
+teleportSection:addButton("Teleport to End", tpToEnd)
+teleportSection:addButton("Teleport to Shop", tpToShop)
+teleportSection:addButton("Teleport to Spawn", tpToSpawn)
+teleportSection:addButton("Teleport to Pit stop", tpToPitStop)
 
-    playerSection:addSlider("Walkspeed", 16, 0, 250, function(val, update)
-        ws = val
-        getChar().Humanoid.WalkSpeed = val
-    end)
-    playerSection:addSlider("Jumpheight", 7.2, 0, 250, function(val, update)
-        jp = val
-        getChar().Humanoid.JumpHeight = val
-    end)
-
-    playerSection:addToggle("Infinite jumps", false, function(val, update)
-        getgenv().infjump = val
-        if val then
-            infiniteJump()
-        end
-    end)
-    playerSection:addToggle("God mode", false, function(val, update)
-        getgenv().god = val
-        if val then
-            god()
-        end
-    end)
-
-
-    local miscPage = venyx:addPage("Misc")
-    local farmSection = miscPage:addSection("Farm")
-    local usefulSection = miscPage:addSection("Useful")
-
-    farmSection:addToggle("Collect coins", false, function(val, update)
-        getgenv().collectcoins = val
-        if val then
-            collectCoins()
-        end
-    end)
-
-    usefulSection:addButton("Get tools", function()
-        getTools()
-    end)
-
-    local trollPage = venyx:addPage("Troll")
-    local cartSection = trollPage:addSection("Cart")
-
-    cartSection:addButton("Carts go forward", function()
-        goForward()
-    end)
-    cartSection:addToggle("Spam carts go forward", false, function(val, update)
-        getgenv().goforward = val
-        if val then
-            task.spawn(function()
-                while getgenv().goforward do
-                    goForward()
-                    wait(.001)
-                end
-            end)
-        end
-    end)
-    cartSection:addButton("Carts go backwards", function()
-        goBackwards()
-    end)
-    cartSection:addToggle("Spam carts go backwards", false, function(val, update)
-        getgenv().gobackwards = val
-        if val then
-            task.spawn(function()
-                while getgenv().gobackwards do
-                    goBackwards()
-                    wait(.001)
-                end
-            end)
-        end
-    end)
-    cartSection:addButton("Stop carts", function()
-        stopCarts()
-    end)
-    cartSection:addToggle("Spam stop carts", false, function(val, update)
-        getgenv().stopCarts = val
-        if val then
-            task.spawn(function()
-                while getgenv().stopCarts do
-                    stopCarts()
-                    wait(.001)
-                end
-            end)
-        end
-    end)
-
-    cartSection:addButton("Lights", function()
-        lightCarts()
-    end)
-    cartSection:addToggle("Spam lights", false, function(val, update)
-        getgenv().lightCarts = val
-        if val then
-            task.spawn(function()
-                while getgenv().lightCarts do
-                    lightCarts()
-                    wait(.001)
-                end
-            end)
-        end
-    end)
-
-    cartSection:addToggle("Spawn bomb carts too?", false, function(val, update)
-        getgenv().bombtoo = val
-    end)
-    cartSection:addButton("Spawn carts", function()
-        spawnCarts()
-    end)
-    cartSection:addToggle("Spam spawn carts", false, function(val, update)
-        getgenv().spamspawn = val
-        if val then
-            task.spawn(function()
-                while getgenv().spamspawn do
-                    spawnCarts()
-                    wait(.001)
-                end
-            end)
-        end
-    end)
-
-    venyx:setTheme("DarkContrast", Color3.fromRGB(14, 14, 14))
-    venyx:SelectPage(venyx.pages[1], true)
+guiSection:addKeybind("Toggle menu", Enum.KeyCode.G, function()
+    venyx:toggle()
 end)
+
+playerSection:addSlider("Walkspeed", 16, 0, 250, function(val, update)
+    ws = val
+    getChar().Humanoid.WalkSpeed = val
+end)
+playerSection:addSlider("Jumpheight", 7.2, 0, 250, function(val, update)
+    jp = val
+    getChar().Humanoid.JumpHeight = val
+end)
+
+playerSection:addToggle("Infinite jumps", false, function(val, update)
+    if val then
+        task.defer(infiniteJump)
+    else
+        infJump:Disconnect()
+    end
+end)
+playerSection:addToggle("God mode", false, function(val, update)
+    if val then
+        task.defer(god)
+    else
+        for k,con in pairs(godCons) do
+            if con then con:Disconnect() end
+        end
+    end
+end)
+
+
+local miscPage = venyx:addPage("Misc")
+local farmSection = miscPage:addSection("Farm")
+local usefulSection = miscPage:addSection("Useful")
+
+farmSection:addToggle("Collect coins", false, function(val, update)
+    if val then
+        task.defer(collectCoins)
+    else
+        task.defer(collectCoins,true)
+    end
+end)
+
+usefulSection:addButton("Get tools", function()
+    task.defer(getTools)
+end)
+
+local trollPage = venyx:addPage("Troll")
+local cartSection = trollPage:addSection("Cart")
+
+local forwardCON, backwardCON, stopCON, lightsCON, spawnCON
+
+cartSection:addButton("Carts go forward", function()
+    task.defer(cartInteract, 'forward')
+end)
+
+
+cartSection:addToggle("Spam carts go forward", false, function(val, update)
+    if val then
+        forwardCON = runUntilFired(cartInteract, 'forward')
+    else
+        forwardCON:Fire()
+    end
+end)
+cartSection:addButton("Carts go backwards", function()
+    task.defer(cartInteract, 'backward')
+end)
+
+
+local backwardsCON
+cartSection:addToggle("Spam carts go backwards", false, function(val, update)
+    if val then
+        backwardsCON = runUntilFired(cartInteract, 'backward')
+    else
+        backwardsCON:Fire()
+    end
+end)
+cartSection:addButton("Stop carts", function()
+    task.defer(cartInteract, 'stop')
+end)
+
+cartSection:addToggle("Spam stop carts", false, function(val, update)
+    if val then
+        stopCON = runUntilFired(cartInteract, 'stop')
+    else
+        stopCON:Fire()
+    end
+end)
+
+cartSection:addButton("Lights", function()
+    task.defer(cartInteract, 'lightbutton')
+end)
+
+cartSection:addToggle("Spam lights", false, function(val, update)
+    if val then
+        lightsCON = runUntilFired(cartInteract, 'lightbutton')
+    else
+        lightsCON:Fire()
+    end
+end)
+
+cartSection:addToggle("Spawn bomb carts too?", false, function(val, update)
+    getgenv().bombtoo = val
+end)
+cartSection:addButton("Spawn carts", function()
+    task.defer(spawnCarts)
+end)
+
+cartSection:addToggle("Spam spawn carts", false, function(val, update)
+    if val then
+        spawnCON = runUntilFired(spawnCarts)
+    else
+        spawnCON:Fire()
+    end
+end)
+
+venyx:setTheme("DarkContrast", Color3.fromRGB(14, 14, 14))
+venyx:SelectPage(venyx.pages[1], true)
 if not fireclickdetector or not firetouchinterest or not getgenv then
     notify("Not supported", "Your exploit is not supported.", 5)
 end
